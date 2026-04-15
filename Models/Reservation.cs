a8a1d4b731454ea27fa44904227a632308f1a0e4
Konrad Niedziółka <s33680@pjwstk.edu.pl>
2026-04-15 21:20:12 +0200

    Add reservation controller and repair data

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -5,12 +5,12 @@ public class Reservation : IValidatableObject
     public int Id { get; set; }
     public int RoomId { get; set; }
     [Required]
-    public string OrganizerName { get; set; }
+    public string OrganizerName { get; set; } = "";
     [Required]
-    public string Topic { get; set; }
+    public string Topic { get; set; } = "";
     public DateTime Date { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
     [Required]
     public string Status { get; set; } = string.Empty;
     
